2283a15073657a87441868a8b47997ad873c8ac3
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-07 21:51:59 +0400

    Create string_processing.cpp

---
+++ string_processing.cpp
@@ -0,0 +1,15 @@
+#include "string_processing.h"
+
+std::vector<string_view> SplitIntoWords(string_view str)
+{
+    vector<string_view> result;
+    int64_t pos = str.find_first_not_of(" ");
+    const int64_t pos_end = str.npos;
+
+    while (pos != pos_end) {
+        int64_t space = str.find(' ', pos);
+        result.push_back(space == pos_end ? str.substr(pos) : str.substr(pos, space - pos));
+        pos = str.find_first_not_of(" ", space);
+    }
+    return result;
+}
